3fb572a7366f3bd6a8cdbcc3787c602e8072808f
Levangul <150108077+Levangul@users.noreply.github.com>
2024-06-29 19:00:04 -0400

    applied css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/authContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDoorOpen } from '@fortawesome/free-solid-svg-icons';
 import '../css/header.css';
 
 const Header = () => {
@@ -22,20 +24,23 @@ const Header = () => {
           <li><Link to="/" className="nav-item">Home</Link></li>
           <li><Link to="/profile" className="nav-item">Profile</Link></li>
         </ul>
-        <ul className="auth-links">
-          {isAuthenticated ? (
-            <li><button onClick={handleLogout} className="nav-item">Logout</button></li>
-          ) : (
-            <>
-              <li><Link to="/login" className="nav-item">Login</Link></li>
-              <li><Link to="/sign-up" className="nav-item">Sign-up</Link></li>
-            </>
-          )}
-        </ul>
+        {isAuthenticated ? (
+          <ul className="auth-links">
+            <li>
+              <button onClick={handleLogout} className="nav-item logout-button">
+                <FontAwesomeIcon icon={faDoorOpen} />
+              </button>
+            </li>
+          </ul>
+        ) : (
+          <ul className="auth-links">
+            <li><Link to="/login" className="nav-item">Login</Link></li>
+            <li><Link to="/sign-up" className="nav-item">Sign-up</Link></li>
+          </ul>
+        )}
       </nav>
     </header>
   );
 };
 
 export default Header;
-
