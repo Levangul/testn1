deabba62ed2c97b90902404a9c1d036a3d88f73a
Levangul <150108077+Levangul@users.noreply.github.com>
2024-07-11 22:39:46 -0400

    auth details

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -13,7 +13,7 @@ class AuthService {
     isTokenExpired(token) {
         const decoded = decode(token);
         if (decoded.exp < Date.now() / 1000) {
-            localStorage.removeItem('id_token');
+            this.logout();
             return true;
         }
         return false;
